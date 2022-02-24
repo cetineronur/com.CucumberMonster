ca3251e0be84f79cb8924411d82839e81dd96dd4
onur cetiner <cetineronur44@gmail.com>
2022-02-24 17:43:17 +0100

    update

--- src/test/java/pages/Onur.java
+++ src/test/java/pages/Onur.java
@@ -1,4 +1,24 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Onur {
+
+public Onur(){
+    PageFactory.initElements(Driver.getDriver(),this);
+}
+
+@FindBy(xpath = "//h2[text()='Subscription']")
+    public WebElement subscription;
+
+@FindBy(xpath = "//input[@id='susbscribe_email']")
+    public WebElement emailBox;
+
+@FindBy(xpath = "//div[text()='You have been successfully subscribed!']")
+    public WebElement message;
+
+
 }
