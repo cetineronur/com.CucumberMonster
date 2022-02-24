cda6beede128aed676866446e6f0209bb59396ef
Guenter Senyurt <senyurtguenter@gmail.com>
2022-02-24 17:11:33 +0100

    PageFactory yazildi

--- src/test/java/pages/Gunter.java
+++ src/test/java/pages/Gunter.java
@@ -1,4 +1,12 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Gunter {
+
+    public Gunter(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
