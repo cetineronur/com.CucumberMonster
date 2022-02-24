9bdc1338d0cd0ade149557e27b29cc43aaff1159
salih <salih.kaya.2050@gmail.com>
2022-02-24 16:14:59 +0300

    ilk commit

--- src/test/java/pages/Salih.java
+++ src/test/java/pages/Salih.java
@@ -1,4 +1,24 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Salih {
+    public Salih() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    // mainPage>logo
+    @FindBy(xpath = "//img[@src='/static/images/home/logo.png']")
+    public WebElement mainPageLogoElementi;
+    // mainPage>SignUp/Login
+    @FindBy(xpath = "//a[@href=\"/login\"]")
+    public WebElement mainPageSignUpLoginLinkiElementi;
+    //mainPage>Login>New User Signup!
+    @FindBy(xpath = "//h2[text()='New User Signup!']")
+    public WebElement loginNewUserSignupTextElementi;
+
+
 }
