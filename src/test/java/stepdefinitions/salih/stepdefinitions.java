383ce79e69efe27031589d68a28d91dae3d61a61
salih <salih.kaya.2050@gmail.com>
2022-02-25 18:11:20 +0300

    ikinci gun page ve stepdefinitions

--- src/test/java/stepdefinitions/salih/stepdefinitions.java
+++ src/test/java/stepdefinitions/salih/stepdefinitions.java
@@ -3,6 +3,10 @@ package stepdefinitions.salih;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import pages.Salih;
 import utilities.ConfigReader;
 import utilities.Driver;
@@ -10,7 +14,8 @@ import utilities.Driver;
 public class stepdefinitions<Faker> {
 
     Salih salih=new Salih();    // src/test/java/pages/Salih.java > class'indan  bir obje olusturuldu.
-
+    Actions actions=new Actions(Driver.getDriver());
+    Select select;
 
     @Given("Launch browser, Navigate to {string}")
     public void launch_browser_navigate_to(String URL) {
@@ -30,43 +35,75 @@ public class stepdefinitions<Faker> {
     }
     @Then("Enter name and email address")
     public void enter_name_and_email_address() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       actions.click(salih.userNameBoxElementi)
+               .sendKeys(ConfigReader.getProperty("AECorrectName"))
+               .sendKeys(Keys.TAB)
+               .sendKeys(ConfigReader.getProperty("AECorrectEmail"))
+               .perform();
     }
     @Then("Click {string} button")
     public void click_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+      salih.signUpButtonElementi.click();
+
     }
     @Then("Verify that {string} is visible")
     public void verify_that_is_visible(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    Assert.assertTrue(salih.signUpHesapBilgileriniGoruntuleYazisiElementi.isDisplayed());
     }
     @Then("Fill details: Title, Name, Email, Password, Date of birth")
     public void fill_details_title_name_email_password_date_of_birth() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        actions.click(salih.signUpTitleRadioButtonElementi)
+                        .sendKeys(Keys.TAB)
+                                .sendKeys(ConfigReader.getProperty("AECorrectName"))
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(ConfigReader.getProperty("AECorrectPassword"))
+                .perform();
+        select=new Select(salih.signUpDayDropDownElementi);
+        select.selectByIndex(28);
+        select=new Select(salih.signUpMonthDropDownElementi);
+        select.selectByIndex(2);
+        select=new Select(salih.signUpYearDropDownElementi);
+        select.selectByIndex(30);
+
     }
     @Then("Select checkbox {string}")
     public void select_checkbox(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        salih.signUpNewsletterCheckBoxElementi.click();
+        salih.signUpOptionCheckBoxElementi.click();
     }
     @Then("Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number")
     public void fill_details_first_name_last_name_company_address_address2_country_state_city_zipcode_mobile_number() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        actions.click(salih.signUpFirstNameBoxElementi)
+                .sendKeys("Guru")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Tech")
+                .sendKeys(Keys.TAB)
+                .sendKeys("GuruTechProEducation")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Moonstar Street, 087600 , GuruTechProEducation , Texas ")
+                .perform();
+        select=new Select(salih.signUpCountrydropDownElementi);
+        select.selectByValue("United States");
+        actions.click(salih.signUpStateElementi)
+                .sendKeys("Texas")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Houston")
+                .sendKeys(Keys.TAB)
+                .sendKeys("77007")
+                .sendKeys(Keys.TAB)
+                .sendKeys("+1 281-591-5547")
+                .perform();
+
     }
     @Then("Click 'Create Account button")
     public void click_create_account_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+      salih.signUpCreateAccountButtonElementi.click();
     }
     @Then("Verify that {string} is visible and click {string} button")
     public void verify_that_is_visible_and_click_button(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(salih.accountCreatedTextElementi.isDisplayed());
     }
 
 
