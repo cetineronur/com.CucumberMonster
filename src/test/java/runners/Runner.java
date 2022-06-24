8c6c5711c2d8a5bb1f67caad6d82402dfd5df9f2
onur cetiner <cetineronur44@gmail.com>
2022-06-24 23:45:58 +0200

    update

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,10 +7,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features={"src/test/resources/features"},
+        features={"src/test/resources/features"
+        },
         plugin={"pretty","html:target/Pcucumber-reports1.html",
                 "json:target/cucumber/cucumber.json",
-                "junit:target/cucumber/cucumber.xml"},
+                "junit:target/cucumber/cucumber.xml"
+        },
         glue = "stepdefinitions",
         tags="",
         dryRun = false
